38e9ca73bea600daaecd52f3d0f0b56fc4040c2f
Animesh Chaturvedi <animeshchaturvedi007@gmail.com>
2024-04-25 01:04:30 +0530

    563. Binary Tree Tilt

---
+++ 0500-0599/0563. Binary Tree Tilt/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum;
+    public int findTilt(TreeNode root) {
+        dfs(root);
+        return sum;
+    }
+
+    public int dfs(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        sum += Math.abs(left - right);
+        return root.val + left + right;
+    }
+}
